5b8b4602998606ad67d3588b3517ebcf1f756057
Hamzah Hussain <0hussh59@solent.ac.uk>
2025-10-03 09:46:22 +0100

    Add custom setter to student class

--- src/Student.kt
+++ src/Student.kt
@@ -1,9 +1,17 @@
-class Student (idIn:String,nameIn:String,courseIn:String,markIn:Double){
+class Student (idIn:String,nameIn:String,courseIn:String,markIn:Double) {
 
     val id: String
     val name: String
     val course: String
-    val mark: Double
+
+    var mark: Double = markIn
+        set(newmark) {
+            if (newmark in 0.0..100.00){
+                field = newmark
+
+            }
+        }
+
 
     init {
         id= idIn
